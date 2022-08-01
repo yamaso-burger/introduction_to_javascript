17003d4b7407e8256b926022f6780fd5e8107826
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-01 15:05:01 +0900

    make some modification into prompt_and_alert.js

--- prompt_and_alert.js
+++ prompt_and_alert.js
@@ -1,4 +1,10 @@
-var tweet = prompt("type your tweet:");
-var maxCount = 140;
 
-alert("Your tweet has " + tweet.length + " characters, " + ( maxCount - tweet.length ) + " characters left.");
+var input = prompt("Compose your tweet:");
+var input_len = input.length;
+var tweet_maxLen = 140;
+var left_len = tweet_maxLen - input_len;
+
+alert("You have written " + input_len + " characters, you have " + left_len + " characters left.");
+
+
+//You have written 182 characters, you have -42 characters left.
