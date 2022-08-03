ecab5dedecd1a78e21c9a6e91c878be08919bca3
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-03 12:12:56 +0900

    create mathRandom.js

---
+++ mathRandom.js
@@ -0,0 +1,10 @@
+var fPerson = prompt("Tell the first person's name.");
+var sPerson = prompt("Tell the second person's name.");
+
+var n = Math.random();
+n *= 100;
+n = Math.floor(n) + 1;
+
+alert("The love persentage between " + fPerson + " and " + sPerson + " is: " + n + " %!!");
+
+//generate random number
